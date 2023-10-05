412200d1c2721e5c445c70908d78cfd3981b0dc0
alan-augusto <augustoalan56@gmail.com>
2023-10-05 08:27:57 -0300

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Agiotagem Legal</h1>
+      <p>Uma forma simplificada de organizar suas contas com terceiros. Agora você sabe até quando vai pagar uma dívida.</p>
+    </div>
+  );
+}
+
+export default App;
